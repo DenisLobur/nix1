96357a9241e3d8a0f5d2da415be4b155ee9bce5d
DenysLobur <denismachtalles@gmail.com>
2022-10-20 22:46:41 +0300

    Add prime number checker

---
+++ src/lesson5/Prime.java
@@ -0,0 +1,14 @@
+package lesson5;
+
+public class Prime {
+    public static boolean isPrime(int n, int i) {
+        if (n <= 2)
+            return n == 2;
+        if (n % i == 0)
+            return false;
+        if (i * i > n)
+            return true;
+
+        return isPrime(n, i + 1);
+    }
+}
